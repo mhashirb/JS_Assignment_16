b5fa91677b473b0ba7befd07611c3cec228b1eaf
mhashirb <hashirbaig275@gmail.com>
2024-02-21 13:15:23 -0800

    JS Assignment 16

---
+++ app.js
@@ -0,0 +1,158 @@
+// alert(10);
+// document.write(10);
+// console.log(10);
+// alert(typeof true);
+
+// var a=20;
+// alert(a);
+// var b="faraz";
+// document.write(b);
+// var firstname="Muhammad";
+// var lastname="hashir";
+// console.log(firstname+lastname);
+// document.write(typeof firstname + "" + typeof lastname);
+// // document.write("<b><br>first name is:</b>",firstname + "" + "<b><br>last name is: </b>"+"<i><i>",lastname)
+// var v;
+// console.log(v);
+// var first="Muhammad";
+// var last="Hashir";
+// var age=19
+// var institute="Jawan Pakistan";
+// var cnic=4321183274815;
+// document.write("<br>Hello, My First name is ","<b><b>",first + "and my last name is "+"<b><b>",last,". I am ","<b><b>",age," years old."," I study in ", "<b><b>",institute,". My cnic numer is ","<b><b>",cnic);
+
+//Chapter 1 
+// Question 1
+// alert(10);
+
+// Question 2
+// alert("Error! Please enter a valid password");
+
+// Question 3
+// alert("Welcome to JS Land..\n Happy Coding!");
+ 
+// Question 4
+// alert("Welcome to JS Land..");
+// alert("Happy Coding \n Prevent this page from creating additional dialogs.");
+
+// Question 5
+// console.log("Hello... I can run JS through my web browser's console");
+
+// Chapter 2
+// Question 1
+// var username;
+// document.write(username);
+
+// Question 2
+// var myName="Muhammad hashir baig";
+// document.write(myName); 
+
+// Question 3
+// var message="Hello World";
+// alert(message);
+
+// Question 4
+// var name="Jhone Doe";
+// alert(name);
+// var age=19;
+// alert(age);
+// var course="Certified Mobile Application Development";
+// alert(course);
+
+//Question 5
+// alert("PIZZA\nPIZZ\nPIZ\nPI\nP");
+
+//Question 6
+// var email1="johndoe";
+// var email2="@gmail.com";
+// alert(email1+email2);
+
+// Question 7
+// var book="A smarter \n way to learn Javascript";
+// alert("I am trying to learn from the book "+ book);
+
+// Question 8
+// document.write("Yah! I can write HTML content through Javascript");
+
+// Question 9
+// var style="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(style);
+
+// Chapter 3
+// Question 1
+// var age=19;
+// alert("I am "+age+" years old.");
+
+// Question 2
+// var n=14;
+// alert("You have visited this site "+n+" times.");
+
+// Question 3
+// var birthYear=2004;
+// document.write("My Birth year is "+birthYear);
+// document.write("<br>Datatype of my declare variable is "+typeof birthYear);
+
+//Question 4
+// var vname="John Doe";
+// var protitle="HB Clothing store";
+// var quantity="5 t-shirts";
+// document.write("<b></b>"+vname+" ordered "+"<b></b>"+quantity+" on"+" "+protitle);
+
+// Secong PDF Questions cahpter 1-3
+
+// Chapter 1
+// Question 1
+// var firstname="Muhammad";
+// var lastname=" Hashir";
+// var email="hashir1234@gmail.com";
+// var phoneno="03116785452";
+// var password="jhadgs122";
+// alert(""+firstname+"\n"+lastname+"\n"+email+"\n"+phoneno+"\n"+password);
+
+// Question 2
+// alert("You're Learning Javascript!");
+
+// Question 3
+// alert("My name is hashir");
+
+// Chapter 2
+//Question 1
+// var firstName;
+
+// Question 2
+// var lastName;
+// lastName="Baig";
+
+// Question 3
+// var teamName="Portugal";
+// alert("",teamName);
+
+// Question 4
+// var bestMan="Charlie";
+// bestMan="Chaplin";
+
+//Chapter 3
+// // Question 1
+// var caseQty;
+
+// // Question 2
+// caseQty=144;
+
+// Question 3
+// var num="9";
+
+// Question 4
+// var var1=13;
+// var var2=var1+14+15;
+// alert(var2);
+
+// Question 5
+// var merchTotal=100;
+// var shippingCharge=10;
+// var orderTotal=merchTotal+shippingCharge;
+// alert(orderTotal);
+
+// Question 6
+// var first=10;
+// first=first+20;
+// alert(first);
